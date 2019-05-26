17dbe9e02067490d67262d6337dc113295cb0f47
Tian Pan <puncsky@gmail.com>
2019-05-25 23:52:16 -0700

    add build-static-html

---
+++ src/server/generate-static-html.ts
@@ -0,0 +1,18 @@
+/* tslint:disable:non-literal-fs-path */
+import axios from "axios";
+import config from "config";
+import fs from "fs";
+import { startServer } from "./start-server";
+
+const port = config.get("server.port");
+
+async function generateStaticHtml(): Promise<void> {
+  await startServer();
+  const res = await axios.get(`http://localhost:${port}/`);
+  const html = res.data;
+  fs.writeFileSync(`${__dirname}/../../dist/index.html`, html);
+}
+
+generateStaticHtml().catch(err => {
+  window.console.error(`failed to generate static HTML: ${err}`);
+});
