bb6dabf7469d0ef2cbec1b7a0f50f16a4fafb72f
Tian Pan <puncsky@gmail.com>
2020-08-27 23:15:59 -0700

    split health into smaller ones

--- src/shared/home/health-controller.tsx
+++ src/shared/home/health-controller.tsx
@@ -1,31 +1,8 @@
-import { colors } from "@/shared/common/styles/style-color";
 import { useGetHealth } from "@/shared/home/hooks/use-health";
-import CheckCircleTwoTone from "@ant-design/icons/CheckCircleTwoTone";
-import CloseCircleTwoTone from "@ant-design/icons/CloseCircleTwoTone";
-import LoadingOutlined from "@ant-design/icons/LoadingOutlined";
 import React from "react";
+import { HealthText } from "./components/health-text";
 
 export const HealthController: React.FC = () => {
   const { loading, data, error } = useGetHealth();
-
-  if (loading) {
-    return (
-      <div>
-        <LoadingOutlined /> Checking Status
-      </div>
-    );
-  }
-  if (error) {
-    return (
-      <div>
-        <CloseCircleTwoTone twoToneColor={colors.error} /> Not OK
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <CheckCircleTwoTone twoToneColor={colors.success} /> {data && data.health}
-    </div>
-  );
+  return <HealthText loading={loading} error={!!error} health={data?.health} />;
 };
