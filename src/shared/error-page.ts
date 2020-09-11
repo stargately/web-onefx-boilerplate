591ab621b1d99f68ae4a48b342b673653e0fb844
Jerry.Chen <menglr@live.com>
2020-09-12 00:11:07 +0800

    add error page

---
+++ src/shared/error-page.ts
@@ -0,0 +1,28 @@
+export default `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src 'unsafe-inline'; img-src data:; connect-src 'self'">
+    <title>500 Internal Server Error</title>
+    <style type="text/css" media="screen">
+    </style>
+  </head>
+  <body>
+    <div class="page-container page-container-responsive">
+      <div class="row space-top-8 space-8">
+        <div class="col-5 col-offset-1">
+          <h1 class="text-jumbo">Oops!</h1>
+          <h2>Well, this is unexpected…</h2>
+          <h6>Error code: 500</h6>
+          <p>An error has occurred and we're working to fix the problem! We’ll be up and running
+          shortly.</p>
+        </div>
+        <div class="col-5 col-offset-1 text-center">
+          <img src="./500 Internal Server Error - Airbnb_files/404-Airbnb_final-d652ff855b1335dd3eedc3baa8dc8b69.gif" width="313" height="428" alt="Girl has dropped her ice cream.">
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+`;
