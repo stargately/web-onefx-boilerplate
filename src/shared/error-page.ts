d3639ca0c90e919d8dc84b03e5e0f8a98561a96a
Jerry.Chen <menglr@live.com>
2020-09-12 17:00:02 +0800

    fix error-page styles

--- src/shared/error-page.ts
+++ src/shared/error-page.ts
@@ -4,21 +4,22 @@ export default `
   <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <title>500 Internal Server Error</title>
+    <link rel="stylesheet" href="/error-page.css">
     <style type="text/css" media="screen">
     </style>
   </head>
   <body>
-    <div class="page-container page-container-responsive">
-      <div class="row space-top-8 space-8">
-        <div class="col-5 col-offset-1">
-          <h1 class="text-jumbo">Oops!</h1>
-          <h2>Well, this is unexpected…</h2>
-          <h6>Error code: 500</h6>
-          <p>An error has occurred and we're working to fix the problem! We’ll be up and running
+    <div class="container max-w-4xl mx-auto p-4">
+      <div class="flex flex-col items-center p-5 md:flex-row">
+        <div class="md:p-5">
+          <h1 class="font-bold text-5xl leading-loose">Oops!</h1>
+          <h2 class="text-3xl leading-loose">Well, this is unexpected…</h2>
+          <h6 class="font-medium leading-loose text-gray-700 mb-2">Error code: 500</h6>
+          <p class="text-gray-700 mb-12">An error has occurred and we're working to fix the problem! We’ll be up and running
           shortly.</p>
         </div>
-        <div class="col-5 col-offset-1 text-center">
-          <img src="/error-page.gif" width="313" height="428" alt="Girl has dropped her ice cream.">
+        <div class="flex justify-items-center py-12 md:px-5">
+          <img src="/error-page.gif" alt="Girl has dropped her ice cream.">
         </div>
       </div>
     </div>
