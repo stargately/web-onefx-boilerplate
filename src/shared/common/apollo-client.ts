bc4f66273c707e25492e479b8c84c8fe6a0df0ae
Jerry.Chen <menglr@live.com>
2020-09-12 19:09:21 +0800

    fix dependencies

--- src/shared/common/apollo-client.ts
+++ src/shared/common/apollo-client.ts
@@ -1,6 +1,4 @@
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloClient } from "@apollo/client";
-import { HttpLink } from "apollo-link-http";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import isBrowser from "is-browser";
 import fetch from "isomorphic-unfetch";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
