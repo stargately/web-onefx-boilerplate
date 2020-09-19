af94de5f8e9eea3c68daa713e55f1bc639bb8fc8
Tian Pan <puncsky@gmail.com>
2020-09-18 21:46:03 -0700

    move onefx-auth out to onefx/packages/onefx-auth

--- src/server/start-server.ts
+++ src/server/start-server.ts
@@ -1,8 +1,7 @@
 import config from "config";
 import { Config, Server } from "onefx/lib/server";
 import { setModel } from "@/model";
-import { OnefxAuth } from "@/shared/onefx-auth";
-import { authConfig } from "@/shared/onefx-auth/auth-config";
+import { OnefxAuth, authConfig } from "onefx-auth";
 import { Gateways, setGateways } from "./gateway/gateway";
 import { setMiddleware } from "./middleware";
 import { setServerRoutes } from "./server-routes";
@@ -28,7 +27,7 @@ export async function startServer(): Promise<Server> {
   const server: MyServer = new Server((config as any) as Config) as MyServer;
   server.app.proxy = Boolean(config.get("server.proxy"));
   setGateways(server);
-  server.auth = new OnefxAuth(server, authConfig);
+  server.auth = new OnefxAuth(server.gateways.mongoose, authConfig);
   setMiddleware(server);
   setModel(server);
   setServerRoutes(server);
